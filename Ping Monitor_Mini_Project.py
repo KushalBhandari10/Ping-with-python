ff81e8471fe645eb0ba6cf99204cfc2e2843c4e0
MrKushalRx <kushalmaper@gmail.com>
2025-05-05 09:40:25 +0545

    Add files via upload

---
+++ Ping Monitor_Mini_Project.py
@@ -0,0 +1,17 @@
+import subprocess
+import datetime
+
+website = "google.com"
+file_name = "ping_log.txt"
+#Run ping command(2requests)
+result = subprocess.run(["ping","-n","2",website],capture_output=True,text=True)
+
+#log the file with timestamp
+timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+with open(file_name,"a") as f:
+    f.write(f"==Ping Test at {timestamp} == \n")
+    if result.returncode == 0:
+     f.write(result.stdout)
+    else:
+       f.write(f"Error {result.stderr}")
+print("Ping test logged")
