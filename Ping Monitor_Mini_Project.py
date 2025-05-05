fda0f424854bee037dfbee62d42e256cd9bcd7a5
MrKushalRx <kushalmaper@gmail.com>
2025-05-05 09:44:08 +0545

    Update Ping Monitor_Mini_Project.py

--- Ping Monitor_Mini_Project.py
+++ Ping Monitor_Mini_Project.py
@@ -5,7 +5,8 @@ website = "google.com"
 file_name = "ping_log.txt"
 #Run ping command(2requests)
 result = subprocess.run(["ping","-n","2",website],capture_output=True,text=True)
-
+#for linux/mac
+#result = subprocess.run(["ping", "-c", "2", website], capture_output=True, text=True)
 #log the file with timestamp
 timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 with open(file_name,"a") as f:
